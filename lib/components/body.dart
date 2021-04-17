8e6375e22ed5f55e977d0f413c0ed10affc8c496
PawankumarPK <pk9910765616@gmail.com>
2021-04-18 00:12:39 +0530

    Gridview with product box with different color

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -9,31 +9,69 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: Text(
+              "Women",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Categories(),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridView.builder(
+              itemCount: products.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10.0,
+                  crossAxisSpacing: 10.0,
+                  childAspectRatio: 0.75
+              ),
+              itemBuilder: (context, index) => ItemCard(product: products[index],),
+            ),
+          ))
+        ]);
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final Product product;
+  final Function press;
+
+  const ItemCard({Key key, this.product, this.press}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
+      children: [
+        Container(
+          padding: EdgeInsets.all(kDefaultPadding),
+          //for demo we used fixed , but now we dont need them
+          // height: 180,
+          // width: 160,
+          decoration: BoxDecoration(
+              color: product.color,
+              borderRadius: BorderRadius.circular(16)),
+          child: Image.asset(products[0].image),
+        ),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          padding: const EdgeInsets.symmetric(vertical: kDefaultPadding / 4),
           child: Text(
-            "Women",
-            style: Theme.of(context)
-                .textTheme
-                .headline5
-                .copyWith(fontWeight: FontWeight.bold),
+            //product is out of demo list
+            products[0].title,
+            style: TextStyle(color: kTextLightColor),
           ),
         ),
-        Categories(),
-        Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(kDefaultPadding),
-              height: 180,
-              width: 160,
-              decoration: BoxDecoration(
-                  color: products[0].color,
-                  borderRadius: BorderRadius.circular(16)),
-              child: Image.asset(products[0].image),
-            ),
-          ],
+        Text(
+          "\$1234",
+          style: TextStyle(fontWeight: FontWeight.bold),
         )
       ],
     );
