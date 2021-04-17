0dd757f3dbea5ff5a67fa914cad4ddc88306b831
PawankumarPK <pk9910765616@gmail.com>
2021-04-17 23:39:59 +0530

    product ui box and image added

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:shopping_app/constants.dart';
+import 'package:shopping_app/models/Product.dart';
+
+import 'categories.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -17,64 +21,21 @@ class Body extends StatelessWidget {
                 .copyWith(fontWeight: FontWeight.bold),
           ),
         ),
-        Categories()
-      ],
-    );
-  }
-}
-
-class Categories extends StatefulWidget {
-  @override
-  _CategoriesState createState() => _CategoriesState();
-}
-
-class _CategoriesState extends State<Categories> {
-  List<String> categories = ["Hand bag", "Jewellery", "Footwear", "Dresses"];
-
-  //by default our first item will be selected
-  int selectedBox = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
-      child: SizedBox(
-        height: 25,
-        child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: categories.length,
-            itemBuilder: (context, index) => buildCategory(index)),
-      ),
-    );
-  }
-
-  Widget buildCategory(int index) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          selectedBox = index;
-        });
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        Categories(),
+        Column(
           children: [
-            Text(
-              categories[index],
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: selectedBox == index ? kTextColor : kTextLightColor),
-            ),
             Container(
-              margin: EdgeInsets.only(top: kDefaultPadding / 4),
-              height: 2,
-              width: 30,
-              color: selectedBox == index ? Colors.black : Colors.transparent,
-            )
+              padding: EdgeInsets.all(kDefaultPadding),
+              height: 180,
+              width: 160,
+              decoration: BoxDecoration(
+                  color: products[0].color,
+                  borderRadius: BorderRadius.circular(16)),
+              child: Image.asset(products[0].image),
+            ),
           ],
-        ),
-      ),
+        )
+      ],
     );
   }
 }
