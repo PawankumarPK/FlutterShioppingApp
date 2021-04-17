7bb9bb8bf804ffb61c42bc49bcb6c418870ed093
PawankumarPK <pk9910765616@gmail.com>
2021-04-17 21:55:44 +0530

    app bar done

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shopping_app/constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset("assets/icons/back.svg"),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: SvgPicture.asset(
+                "assets/icons/search.svg",
+                color: kTextColor,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: SvgPicture.asset(
+                "assets/icons/cart.svg",
+                color: kTextColor,
+              ),
+              onPressed: () {}),
+          //for giving equal padding
+          SizedBox(
+            width: kDefaultPadding / 2,
+          )
+        ]);
+  }
+}
