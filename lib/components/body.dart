ee96849438f3cb1d92bdd77cce8f78c3d0283c40
PawankumarPK <pk9910765616@gmail.com>
2021-04-17 23:31:18 +0530

    horizontal scrollbar done

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -5,7 +5,7 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start ,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
@@ -36,32 +36,44 @@ class _CategoriesState extends State<Categories> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 25,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: categories.length,
-          itemBuilder: (context, index) => buildCategory(index)),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+      child: SizedBox(
+        height: 25,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: categories.length,
+            itemBuilder: (context, index) => buildCategory(index)),
+      ),
     );
   }
 
   Widget buildCategory(int index) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            categories[index],
-            style: TextStyle(fontWeight: FontWeight.bold, color: kTextColor),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: kDefaultPadding / 4),
-            height: 2,
-            width: 30,
-            color: Colors.black,
-          )
-        ],
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedBox = index;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              categories[index],
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: selectedBox == index ? kTextColor : kTextLightColor),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: kDefaultPadding / 4),
+              height: 2,
+              width: 30,
+              color: selectedBox == index ? Colors.black : Colors.transparent,
+            )
+          ],
+        ),
       ),
     );
   }
