edc2c7892ade97da84727dde160cd3f23bc323ff
PawankumarPK <pk9910765616@gmail.com>
2021-04-17 23:16:46 +0530

    horizontal scrollbar done

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -10,13 +10,47 @@ class Body extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
           child: Text(
             "Women",
-            style: Theme.of(context)
+            style: Theme
+                .of(context)
                 .textTheme
                 .headline5
                 .copyWith(fontWeight: FontWeight.bold),
           ),
-        )
+        ),
+        Categories()
       ],
+    );
+  }
+}
+
+class Categories extends StatefulWidget {
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ["Hand bag", "Jewellery", "Footwear", "Dresses"];
+
+  //by default our first item will be selected
+  int selectedBox = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (context, index) => buildCategory(index)
+      ),
+    );
+  }
+
+  Widget buildCategory(int index) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Text(
+          categories[index]),
     );
   }
 }
