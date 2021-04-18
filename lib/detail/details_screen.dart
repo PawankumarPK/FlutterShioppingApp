ef9048e034b9733c77c539fad9782e1b0162b49d
PawankumarPK <pk9910765616@gmail.com>
2021-04-18 13:04:43 +0530

    detail product color background change done and app bar done

--- lib/detail/details_screen.dart
+++ lib/detail/details_screen.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shopping_app/constants.dart';
 import 'package:shopping_app/models/Product.dart';
 
 class DetailScreen extends StatelessWidget {
-
   final Product product;
 
   const DetailScreen({Key key, this.product}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      //each product have a color
+      backgroundColor: product.color,
+      appBar: buildAppBar(context),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: product.color,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          "assets/icons/back.svg",
+          color: Colors.white,
+        ),
+        onPressed: () => Navigator.pop(context),
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset("assets/icons/search.svg"),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset("assets/icons/cart.svg"),
+          onPressed: () {},
+        ),
+        SizedBox(
+          width: kDefaultPadding / 2,
+        )
+      ],
+    );
   }
 }
