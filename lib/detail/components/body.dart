40e8774c4a6b74073722eb75d0d547971e3dc03a
PawankumarPK <pk9910765616@gmail.com>
2021-04-18 20:45:49 +0530

    add product title with image

--- lib/detail/components/body.dart
+++ lib/detail/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:shopping_app/constants.dart';
+import 'package:shopping_app/detail/components/product_title_with_image.dart';
 import 'package:shopping_app/models/Product.dart';
 
 class Body extends StatelessWidget {
@@ -10,7 +11,9 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery
+        .of(context)
+        .size;
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
@@ -20,60 +23,116 @@ class Body extends StatelessWidget {
               children: <Widget>[
                 Container(
                   margin: EdgeInsets.only(top: size.height * 0.3),
-                  height: 500,
+                  padding: EdgeInsets.only(
+                      top: size.height * 0.12,
+                      left: kDefaultPadding,
+                      right: kDefaultPadding),
+                  //height: 500,
                   decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(24),
-                          topRight: Radius.circular(24))),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Aristocratic Hand Bag",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        product.title,
-                        style: Theme.of(context).textTheme.headline4.copyWith(
-                            color: Colors.white, fontWeight: FontWeight.bold),
-                      ),
-                      //SizedBox(height: kDefaultPadding),
-                      Row(
-                        children: <Widget>[
-                          RichText(
-                              text: TextSpan(children: [
-                            TextSpan(text: "Price\n"),
-                            TextSpan(
-                                text: "\$${product.price}",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .headline4
-                                    .copyWith(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold))
-                          ])),
-                          SizedBox(width: kDefaultPadding),
-                          Expanded(
-                            child: Image.asset(
-                              product.image,
-                              fit: BoxFit.fill,
-                            ),
-                          )
-                        ],
+                          topRight: Radius.circular(24)
                       )
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      ColorAndSize(product: product,)
                     ],
                   ),
-                )
+                ),
+                ProductTitleWithImage(product: product)
               ],
             ),
           )
         ],
       ),
     );
+  }
+}
+
+class ColorAndSize extends StatelessWidget {
+  const ColorAndSize({
+    Key key,
+    this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Color"),
+              Row(
+                children: <Widget>[
+                  ColorDot(
+                    color: Color(0xFF356C95),
+                    isSelected: true,
+                  ),
+                  ColorDot(color: Color(0xFFF8C078)),
+                  ColorDot(color: Color(0xFFA29B9B))
+                ],
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: RichText(
+            text: TextSpan(
+                style: TextStyle(color: kTextColor),
+                children: [
+                  TextSpan(text: "Size\n"),
+                  TextSpan(
+                      text: "${product.size} cm",
+                      style: Theme
+                          .of(context)
+                          .textTheme
+                          .headline5
+                          .copyWith(
+                          fontWeight: FontWeight.bold))
+                ]),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ColorDot extends StatelessWidget {
+  final Color color;
+  final bool isSelected;
+
+  const ColorDot({Key key,
+    this.color,
+    //by default isSelected is false
+    this.isSelected = false})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin:
+      EdgeInsets.only(top: kDefaultPadding / 4, right: kDefaultPadding / 2),
+      padding: EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: isSelected ? color : Colors.transparent)),
+      child: DecoratedBox(
+        decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<bool>('isSelected', isSelected));
   }
 }
