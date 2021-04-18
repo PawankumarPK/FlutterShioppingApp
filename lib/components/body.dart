bc94b0e699a7cf98556de5353edf28315c424c56
PawankumarPK <pk9910765616@gmail.com>
2021-04-18 12:45:03 +0530

    refesh code

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:shopping_app/constants.dart';
+import 'package:shopping_app/detail/details_screen.dart';
 import 'package:shopping_app/models/Product.dart';
 
 import 'categories.dart';
+import 'item_card.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -15,7 +17,8 @@ class Body extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
             child: Text(
               "Women",
-              style: Theme.of(context)
+              style: Theme
+                  .of(context)
                   .textTheme
                   .headline5
                   .copyWith(fontWeight: FontWeight.bold),
@@ -24,58 +27,28 @@ class Body extends StatelessWidget {
           Categories(),
           Expanded(
               child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GridView.builder(
-              itemCount: products.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 10.0,
-                  crossAxisSpacing: 10.0,
-                  childAspectRatio: 0.75
-              ),
-              itemBuilder: (context, index) => ItemCard(product: products[index],),
-            ),
-          ))
+                padding: const EdgeInsets.all(8.0),
+                child: GridView.builder(
+                  itemCount: products.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10.0,
+                      crossAxisSpacing: 10.0,
+                      childAspectRatio: 0.75),
+                  itemBuilder: (context, index) =>
+                      ItemCard(
+                        product: products[index],
+                        press: () =>
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        DetailScreen(
+                                          product: products[index],
+                                        ))),
+                      ),
+                ),
+              ))
         ]);
-  }
-}
-
-class ItemCard extends StatelessWidget {
-  final Product product;
-  final Function press;
-
-  const ItemCard({Key key, this.product, this.press}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          child: Container(
-            padding: EdgeInsets.all(kDefaultPadding),
-            //for demo we used fixed , but now we dont need them
-            // height: 180,
-            // width: 160,
-            decoration: BoxDecoration(
-                color: product.color,
-                borderRadius: BorderRadius.circular(16)),
-            child: Image.asset(product.image),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: kDefaultPadding / 4),
-          child: Text(
-            //product is out of demo list
-            product.title,
-            style: TextStyle(color: kTextLightColor),
-          ),
-        ),
-        Text(
-          "\$${product.price}",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        )
-      ],
-    );
   }
 }
---
+++ lib/detail/details_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/models/Product.dart';
+
+class DetailScreen extends StatelessWidget {
+
+  final Product product;
+
+  const DetailScreen({Key key, this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
