e479076175eca6dc1df5a292d58a568b96e24d99
PawankumarPK <pk9910765616@gmail.com>
2021-04-18 21:14:18 +0530

    increment and decrement number of product done

--- lib/detail/components/body.dart
+++ lib/detail/components/body.dart
@@ -5,6 +5,7 @@ import 'package:shopping_app/detail/components/product_title_with_image.dart';
 import 'package:shopping_app/models/Product.dart';
 
 import 'color_and_size.dart';
+import 'description.dart';
 
 class Body extends StatelessWidget {
   final Product product;
@@ -13,9 +14,7 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery
-        .of(context)
-        .size;
+    Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
@@ -34,12 +33,14 @@ class Body extends StatelessWidget {
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(24),
-                          topRight: Radius.circular(24)
-                      )
-                  ),
+                          topRight: Radius.circular(24))),
                   child: Column(
                     children: <Widget>[
-                      ColorAndSize(product: product,)
+                      ColorAndSize(product: product),
+                      Description(
+                        product: product,
+                      ),
+                      CartCounter()
                     ],
                   ),
                 ),
@@ -48,6 +49,64 @@ class Body extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class CartCounter extends StatefulWidget {
+  int numOfItems = 1;
+
+  @override
+  _CartCounterState createState() => _CartCounterState();
+}
+
+class _CartCounterState extends State<CartCounter> {
+  int numOfItems = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        buildOutlineButton(
+          icon: Icons.remove,
+          press: () {
+            if (numOfItems > 1) {
+              setState(() {
+                numOfItems--;
+              });
+            }
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding / 2),
+          child: Text(
+            //if our items is less than 10 then it show 01,02 like that
+            numOfItems.toString(),
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+        buildOutlineButton(
+          icon: Icons.add,
+          press: () {
+            setState(() {
+              numOfItems++;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  SizedBox buildOutlineButton({IconData icon, Function press}) {
+    return SizedBox(
+      width: 40,
+      height: 32,
+      child: OutlineButton(
+        padding: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(13)),
+        onPressed: press,
+        child: Icon(icon),
       ),
     );
   }
