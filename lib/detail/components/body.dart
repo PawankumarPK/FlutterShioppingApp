3cdd6908fe9b96ee136a7619e2fd0a397ff9abca
PawankumarPK <pk9910765616@gmail.com>
2021-04-18 14:39:08 +0530

    product image added and price also

--- lib/detail/components/body.dart
+++ lib/detail/components/body.dart
@@ -41,6 +41,30 @@ class Body extends StatelessWidget {
                         product.title,
                         style: Theme.of(context).textTheme.headline4.copyWith(
                             color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      //SizedBox(height: kDefaultPadding),
+                      Row(
+                        children: <Widget>[
+                          RichText(
+                              text: TextSpan(children: [
+                            TextSpan(text: "Price\n"),
+                            TextSpan(
+                                text: "\$${product.price}",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline4
+                                    .copyWith(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold))
+                          ])),
+                          SizedBox(width: kDefaultPadding),
+                          Expanded(
+                            child: Image.asset(
+                              product.image,
+                              fit: BoxFit.fill,
+                            ),
+                          )
+                        ],
                       )
                     ],
                   ),
